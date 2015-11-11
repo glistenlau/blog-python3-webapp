a7d753de73b4ef63b0c26d4b21b88759c9e04292
Yi Liu <purplesky8985@gmail.com>
2015-11-10 22:32:04 -0600

    reconstructed blog edit page using react

--- www/static/js/manage_blog_edit.js
+++ www/static/js/manage_blog_edit.js
@@ -1,41 +1,89 @@
-    var
-        ID = '{{ id }}',
-        action = '{{ action }}';
-    function initVM(blog) {
-        var vm = new Vue({
-            el: '#vm',
-            data: blog,
-            method: {
-                submit: function(event) {
-                    event.preventDefault();
-                    var $form = $('vm').find('form');
-                    $form.postJSON(action, this.$data, function(err, r) {
-                        if (err) {
-                            $form.showFormError(err);
-                        } else {
-                            return location.assign('/api/blogs/' + r.id);
-                        }
-                    });
-                }
-            }
-        });
-        $('#vm').show();
-    }
-    $(function() {
-        if(ID) {
-            getJSON('/api/blogs/' + ID, function(err, blog) {
-                if(err) {
-                    return fatal(err)
-                }
-                $('#loading'.hide());
-                initVM(blog);
-            });
-        } else {
-            $('#loading').hide();
-            initVM({
-                name: '',
-                summary: '',
-                content: ''
-            });
+var EditBox = React.createClass({
+    getInitialState: function() {
+        return {
+            name: '',
+            summary: '',
+            content: ''
         }
-    });
+    },
+
+    handleChange: function() {
+        this.setState({
+            name: this.refs.articleName.value,
+            summary: this.refs.articleSummary.value,
+            content: this.refs.articleContent.value
+        });
+    },
+
+    componentDidMount: function() {
+        this.loadArticle();
+    },
+
+    handleSubmit: function(event) {
+        event.preventDefault();
+        $.post(this.props.action, $('#edit-form').serializeArray())
+            .done(function(res) {
+                location.assign('/blog/' + res.id);
+            }.bind(this));
+    },
+
+    loadArticle: function() {
+        if (this.props.ID) {
+            $.get('/api/blogs/' + this.props.ID)
+                .done(function(blog) {
+                    this.setState({
+                        name: blog.name,
+                        summary: blog.summary,
+                        content: blog.content
+                    })
+                }.bind(this));
+        }
+    },
+
+    render: function() {
+        return (
+            <div className="container">
+                <form id="edit-form" onSubmit={this.handleSubmit}>
+                    <div className="form-group">
+                        <label>Title</label>
+                            <textarea
+                                name="name"
+                                className="form-control"
+                                onChange={this.handleChange}
+                                placeholder="Please Enter the title"
+                                value={this.state.name}
+                                ref="articleName" />
+                    </div>
+                    <div className="form-group">
+                        <label>Summary</label>
+                            <textarea
+                                name="summary"
+                                className="form-control"
+                                onChange={this.handleChange}
+                                placeholder="Please Enter the summary"
+                                value={this.state.summary}
+                                rows="3"
+                                ref="articleSummary" />
+                    </div>
+                    <div className="form-group">
+                        <label>Content</label>
+                            <textarea
+                                name="content"
+                                className="form-control"
+                                onChange={this.handleChange}
+                                placeholder="Please Enter the content"
+                                value={this.state.content}
+                                rows="10"
+                                ref="articleContent" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <EditBox ID={window.ID} action={window.action} />,
+    document.getElementById("blog_edit")
+);
