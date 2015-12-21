dfda4b6868db12b3f67870d4fb81a804f8b80337
Yi Liu <purplesky8985@gmail.com>
2015-12-21 12:48:08 -0600

    speed page loading

---
+++ config_override.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configuration
+'''
+
+__author__ = 'YiLIU'
+
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
--- handlers.py
+++ handlers.py
@@ -10,13 +10,15 @@ import time
 import json, logging, hashlib, base64, asyncio
 from aiohttp import web
 from coroweb import get, post
+from models import User, Comment, Blog, next_id
 from apis import Page, APIValueError, APIResourceNotFoundError, APIError, \
     APIPermissionError
 from models import User, Comment, Blog, next_id
+from config import configs
 from markdown2 import markdown
 
 COOKIE_NAME = 'awesession'
-_COOKIE_KEY = 'Awesome'
+_COOKIE_KEY = configs.session.secret
 
 
 def check_admin(request):
